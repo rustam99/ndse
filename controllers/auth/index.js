3d77dc266b9a5c7fa3ed6f1788f0685bc5826862
Giliazev Rustam <giliazev_r@rusklimat.ru>
2023-09-25 23:48:22 +0300

    update auth controller

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,7 +1,59 @@
-import { signup } from './signup.js'
-import { signin } from './signin.js'
+import passport from 'passport'
+import { responseErrors } from '../../utils/responseErrors.js'
+import { UserModule } from '../../services/UserModule/index.js'
 
 export const AuthController = {
-  signup,
-  signin,
+  signup: async (request, response) => {
+    try {
+      const user = await UserModule.create(request.body)
+
+      if (user instanceof Error) {
+        return responseErrors.badRequest(response, user.message)
+      }
+
+      return response.json({
+        status: 'ok',
+        data: user,
+      })
+    } catch (error) {
+      return responseErrors.internal(response, error)
+    }
+  },
+  signin: (request, response, next) => {
+    passport.authenticate('local', {}, (error, user, info) => {
+      if (error) {
+        return responseErrors.internal(response, error?.message)
+      }
+
+      if (info?.message) {
+        return responseErrors.unauthorized(response, info.message)
+      }
+
+      request.login(user, (error) => {
+        if (error) {
+          return responseErrors.internal(response, error?.message)
+        }
+
+        const prevSession = request.session
+
+        request.session.regenerate((error) => {
+          if (error) {
+            return responseErrors.internal(response, error?.message)
+          }
+
+          Object.assign(request.session, prevSession)
+
+          return response.json({
+            status: 'ok',
+            data: {
+              id: user._id,
+              email: user.email,
+              name: user.name,
+              contactPhone: user.contactPhone,
+            },
+          })
+        })
+      })
+    })(request, response, next)
+  },
 }
